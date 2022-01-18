ca6a265bb8ee1f40b68d8bac78029e31c1377d05
Rafael Pereira de Oliveira <rafael.oliveira4@alunos.unis.edu.br>
2022-01-17 22:13:15 -0300

    Refatorando contador com Listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
-var currentNumberWrapper = document.getElementById('currentNumber')
+// var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0
+var addBtn = document.querySelector('button[name="adicionar"]')
+var subBtn = document.querySelector('button[name="subtrair"]')
+var textResult = document.querySelector('#currentNumber')
 
-function increment() {
-	currentNumber = currentNumber + 1 
-	currentNumberWrapper.innerHTML = currentNumber
-}
+// 		## Chamando função com onClick no HTML
+// function increment() {
+// 	currentNumber = currentNumber + 1 
+// 	currentNumberWrapper.innerHTML = currentNumber
+// }
+// 
+// function decrement() {
+// 	currentNumber = currentNumber - 1
+// 	currentNumberWrapper.innerHTML = currentNumber
+// }
 
-function decrement() {
-	currentNumber = currentNumber - 1
-	currentNumberWrapper.innerHTML = currentNumber
-}
+//		## Utilizando Listener
+
+addBtn.addEventListener('click', function(event) {
+	currentNumber = currentNumber + 1
+  textResult.textContent = currentNumber
+})
+
+subBtn.addEventListener('click', {
+	handleEvent: function(event) {
+		currentNumber = currentNumber - 1
+		textResult.textContent = currentNumber
+	}
+})
